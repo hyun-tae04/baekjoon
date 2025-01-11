ee7763ce251c6943fb9028b51ebd896695c9593b
hyun-tae04 <79366897+hyun-tae04@users.noreply.github.com>
2025-01-11 13:35:23 +0900

    [Silver IV] Title: 카드2, Time: 0 ms, Memory: 1128 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.c
+++ 백준/Silver/2164. 카드2/카드2.c
@@ -1,15 +1,9 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-  int a, cnt = 0;
+  int a;
   scanf("%d", &a);
-  int number = a;
-  while(number > 0){
-    number = number / 2;
-    cnt++;
-  }
-  int k = a - pow(2, cnt-1);
-  if (k == 0) printf("%d", a);
-  else  printf("%d", k*2);
+  int cnt = log2(a);
+  printf("%d", (a == (1 << cnt)) ? a : (a - (1 << cnt)) * 2);
   return 0;
 }
